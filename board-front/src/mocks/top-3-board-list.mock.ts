586d11c87a2a4a340e6e239b366417a5555f61bc
Kimkyunghoon1117 <hoon-1117@naver.com>
2024-04-03 14:23:47 +0900

    component : top3 list item

---
+++ board-front/src/mocks/top-3-board-list.mock.ts
@@ -0,0 +1,42 @@
+import { BoardListItem } from "types/interface";
+
+const top3BoardListMock: BoardListItem[] =[
+    {
+        boardNumber : 1,
+        title: "test1",
+        content: "test1",
+        boardTitleImage: 'https://ifh.cc/g/d2XNHO.jpg',
+        favoriteCount: 0,
+        commentCount: 0,
+        viewCount: 0,
+        writeDatetime: "test",
+        writerNickname: "test",
+        writerProfileImage: 'https://ifh.cc/g/d2XNHO.jpg',
+    },
+    {
+        boardNumber : 2,
+        title: "test1",
+        content: "test1",
+        boardTitleImage: 'https://ifh.cc/g/d2XNHO.jpg',
+        favoriteCount: 0,
+        commentCount: 0,
+        viewCount: 0,
+        writeDatetime: "test",
+        writerNickname: "test",
+        writerProfileImage: 'https://ifh.cc/g/d2XNHO.jpg',
+    },
+    {
+        boardNumber : 3,
+        title: "test1",
+        content: "test1",
+        boardTitleImage: 'https://ifh.cc/g/d2XNHO.jpg',
+        favoriteCount: 0,
+        commentCount: 0,
+        viewCount: 0,
+        writeDatetime: "test",
+        writerNickname: "test",
+        writerProfileImage: 'https://ifh.cc/g/d2XNHO.jpg',
+    },
+];
+
+export default top3BoardListMock;
