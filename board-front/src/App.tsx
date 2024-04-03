b979944298af323f2d73e129bd08ffd04e0f8c1d
Kimkyunghoon1117 <hoon-1117@naver.com>
2024-04-04 01:15:59 +0900

    component input a

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -5,6 +5,7 @@ import BoardItem from 'components/boardItem';
 import Top3Item from 'components/Top3Item';
 import CommentItem from 'components/CommentItem';
 import FavoriteItem from 'components/FavoriteItem';
+import InputBox from 'components/inputBox';
 
 function App() {
   return (
@@ -13,12 +14,13 @@ function App() {
       {/* <div style={{display: 'flex', justifyContent:'center', gap:'24px'}}>
         {top3BoardListMock.map(top3ListItem => <Top3Item top3ListItem={top3ListItem}/>)}
       </div> */}
-      <div style={{padding:'0 20px', display: 'flex', flexDirection: 'column', gap:'30px'}}>
+      {/* <div style={{padding:'0 20px', display: 'flex', flexDirection: 'column', gap:'30px'}}>
         {commentListMock.map(commentListItem => <CommentItem commentListItem={commentListItem}/>)}
       </div>
       <div style={{padding:'0 20px', display: 'flex', columnGap:'30px', rowGap: '20px'}}>
         {favoriteListMock.map(favoriteListItem => <FavoriteItem favoriteListItem={favoriteListItem}/>)}
-      </div>
+      </div> */}
+      <InputBox />
     </>
   );
 }
