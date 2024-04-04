f316b1fa8056cb58eb0da7d4f92109ac49e066ab
Kimkyunghoon1117 <hoon-1117@naver.com>
2024-04-05 00:55:20 +0900

    component footer

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,32 +1,26 @@
-import React, { useState } from 'react';
+import Footer from 'layouts/Footer';
 import './App.css';
-import { latestBoardListMock, top3BoardListMock, commentListMock, favoriteListMock } from 'mocks';
-import BoardItem from 'components/boardItem';
-import Top3Item from 'components/Top3Item';
-import CommentItem from 'components/CommentItem';
-import FavoriteItem from 'components/FavoriteItem';
-import InputBox from 'components/inputBox';
 
 function App() {
 
-  const[value, setValue]=useState<string>('');
-
   return (
     <>
-      {/* {latestBoardListMock.map(boardListItem => <BoardItem boardListItem={boardListItem}/>)} */}
-      {/* <div style={{display: 'flex', justifyContent:'center', gap:'24px'}}>
-        {top3BoardListMock.map(top3ListItem => <Top3Item top3ListItem={top3ListItem}/>)}
-      </div> */}
-      {/* <div style={{padding:'0 20px', display: 'flex', flexDirection: 'column', gap:'30px'}}>
-        {commentListMock.map(commentListItem => <CommentItem commentListItem={commentListItem}/>)}
-      </div>
-      <div style={{padding:'0 20px', display: 'flex', columnGap:'30px', rowGap: '20px'}}>
-        {favoriteListMock.map(favoriteListItem => <FavoriteItem favoriteListItem={favoriteListItem}/>)}
-      </div> */}
-      <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요' value={value} error={true} 
-        setValue={setValue} message='testtes'/>
+      <Footer />
     </>
   );
 }
 
 export default App;
+
+{/* {latestBoardListMock.map(boardListItem => <BoardItem boardListItem={boardListItem}/>)} */}
+{/* <div style={{display: 'flex', justifyContent:'center', gap:'24px'}}>
+  {top3BoardListMock.map(top3ListItem => <Top3Item top3ListItem={top3ListItem}/>)}
+</div> */}
+{/* <div style={{padding:'0 20px', display: 'flex', flexDirection: 'column', gap:'30px'}}>
+  {commentListMock.map(commentListItem => <CommentItem commentListItem={commentListItem}/>)}
+</div>
+<div style={{padding:'0 20px', display: 'flex', columnGap:'30px', rowGap: '20px'}}>
+  {favoriteListMock.map(favoriteListItem => <FavoriteItem favoriteListItem={favoriteListItem}/>)}
+</div> */}
+{/* <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요' value={value} error={true} 
+  setValue={setValue} message='testtes'/> */}
