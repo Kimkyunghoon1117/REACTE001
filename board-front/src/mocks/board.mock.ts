7046601e311ddbd3d4cbc928a226db9096303064
Kimkyunghoon1117 <hoon-1117@naver.com>
2024-04-27 03:29:13 +0900

    board detail page event a

---
+++ board-front/src/mocks/board.mock.ts
@@ -0,0 +1,16 @@
+import { Board } from "types/interface";
+
+const boardMock: Board = {
+    boardNumber : 1,
+    title: 'test1 test1 test1 test1 test1 test1 test1 test1 test1 test1',
+    content: 'test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1',
+    boardImageList: ['https://img2.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/24/koreadognews/20210524023123507ebyk.png',
+    'https://img2.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/24/koreadognews/20210524023123507ebyk.png'
+    ],
+    writeDatetime : '2022. 05. 12',
+    writerEmail: 'email@email.com',
+    writerNickname: 'test1test1test1',
+    writerProfileImage: 'https://img2.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/24/koreadognews/20210524023123507ebyk.png',
+}
+
+export default boardMock;
