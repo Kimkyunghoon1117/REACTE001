2e340f9904fea4c3544e060a681b666f653cc189
Kimkyunghoon1117 <hoon-1117@naver.com>
2024-05-08 20:45:15 +0900

    board detail favorite, comment, board delete api a

--- board-front/src/apis/response/board/index.ts
+++ board-front/src/apis/response/board/index.ts
@@ -1,11 +1,17 @@
 import GetBoardResponseDto from './get-borad.response.dto';
 import PostBoardResponseDto from './post-board.response.dto';
+import PostCommentResponseDto from './post-comment.response.dto';
+import PutFavoriteResponseDto from './put_favorite.response.dto';
+import DeleteBoardResponseDto from './delete-board.response.dto';
 import GetCommentListResponseDto from './get-comment-list.response.dto';
 import GetFavoriteListResponseDto from './get-favorite-list.response.dto';
 import IncreaseViewCountResponseDto from './increase-view-count.response.dto';
 export type {
     GetBoardResponseDto,
     PostBoardResponseDto,
+    PostCommentResponseDto,
+    PutFavoriteResponseDto,
+    DeleteBoardResponseDto,
     GetCommentListResponseDto,
     GetFavoriteListResponseDto,
     IncreaseViewCountResponseDto
